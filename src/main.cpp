3b7dfcbcfe32febb54f2dfe2a3df66bf6d8386a4
Boyan Atanasov <b.atanasov@icloud.com>
2020-01-14 00:30:19 +0000

    formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
-#include <vector>
-#include <fstream>
 #include <map>
 
 #include "mnist.h"
 
-int main() {
+int main()
+{
     mnist::mnist_data images;
     // print prompt
     std::cout << "Enter images dataset filepath:" << std::endl;
     std::string filepath;
     std::getline(std::cin, filepath);
-    if(!mnist::dataset_load(images, filepath)) return 1;
+    if (!mnist::dataset_load(images, filepath))
+    { return 1; }
     mnist::dataset_info(images);
 
     mnist::mnist_data labels;
     // print prompt
     std::cout << "Enter images dataset filepath:" << std::endl;
     std::getline(std::cin, filepath);
-    if(!mnist::dataset_load(labels, filepath)) return 1;
+    if (!mnist::dataset_load(labels, filepath))
+    { return 1; }
     mnist::dataset_info(labels);
 }
